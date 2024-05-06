46fbd2898f765a2c4ea56d168ce65aca838753b7
ArianneAlonso <ariannealonso8@gmail.com>
2024-05-06 10:14:20 -0300

    se realizó la actividad de rango de edad

--- assets/js/rangoEdad.js
+++ assets/js/rangoEdad.js
@@ -1,1 +1,11 @@
-let
+let edad = prompt ("Introduce una edad:");
+
+if (edad >= 0 && edad <= 12) {
+    console.log ("niño");
+} else if (edad >= 13 && edad <= 19) {
+    console.log ("adolescente");
+} else if (edad >= 20 && edad <= 59) {
+    console.log ("adulto");
+} else {
+    console.log ("adulto mayor")
+}
