0374b2bea24f11d539453cd857319cb32b40bcb7
ArianneAlonso <ariannealonso8@gmail.com>
2024-05-06 10:04:04 -0300

    se realizó la actividad de dias de la semana

--- assets/js/diaSemana.js
+++ assets/js/diaSemana.js
@@ -1,1 +1,19 @@
-let 
+let dia = prompt ("Introduce un numero:");
+
+if (dia == 1) {
+    console.log ("es lunes");
+} else if (dia == 2) {
+    console.log ("es martes");
+} else if (dia == 3) {
+    console.log ("es miercoles");
+} else if (dia == 4) {
+    console.log ("es jueves");
+} else if (dia == 5) {
+    console.log ("es viernes");
+} else if (dia == 6) {
+    console.log ("es sabado");
+} else if (dia == 7) {
+    console.log ("es domingo");
+} else {
+    console.log ("no hay dia con ese numero")
+}
